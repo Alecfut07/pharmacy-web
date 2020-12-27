59d6c6b23ebdc1aee05961c5909eee76231ba672
Alecfut07 <ortega.cisneros.alec@gmail.com>
2020-12-26 23:28:33 -0800

    Arreglar y aplicar el funcionamiento de crear y actualizar un producto

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,32 @@ const axios = require('axios');
 router.get('/', function(req, res, next) {
   axios.get('http://localhost:3001/products').then(resp => {
     res.render('products', { products: resp.data.data })
+    // console.log(resp);
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  axios.get('http://localhost:3001/categories').then(resp => {
+    res.render('create-product', { categories: resp.data.data })
     console.log(resp);
+  });
+});
+
+router.post('/update', function(req, res, next) {
+  const { productId, nameProduct, categoryId, priceProduct } = req.body;
+  console.log(productId);
+  console.log(nameProduct);
+  console.log(categoryId);
+  console.log(priceProduct);
+  axios.put(`http://localhost:3001/products/${productId}`, {
+    name: nameProduct,
+    details: 'asdf',
+    category_id: categoryId,
+    price: 100,
+  }).then((resp) => {
+    res.redirect('/products');
+  }).catch((err) => {
+    console.log(err);
   });
 });
 
@@ -27,16 +52,20 @@ router.get('/:id', function(req, res, next) {
     })
     // console.log('Product:', productResp);
     // console.log('Categories:', categoriesResp);
-  }));
+  })).catch((err) => {
+    console.log(err);
+  });
 });
 
 hbs.registerHelper('isSelectedCategory', function (category, selectedCategory) {
   return category.id === selectedCategory.id;
 });
 
-// router.get('/new', function(req, res, next) {
+// router.post('/', function(req, res, next) {
 //   axios.get('http://localhost:3001/categories').then(resp => {
-//     res.render('new-product', { category: resp.data.data })
+//     res.render('create-product', { categories: resp.data.data })
+//     console.log(resp);
 //   });
 // });
+
 module.exports = router;

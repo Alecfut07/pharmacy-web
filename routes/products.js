3b382a1503911d02387d5842b967a6a9fcbd3c3d
Alecfut07 <ortega.cisneros.alec@gmail.com>
2020-12-26 17:49:45 -0800

    Crear un form para los detalles del producto pero falta el boton de guardar para actualizar el producto

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var hbs = require('hbs');
 var router = express.Router();
 
 /* GET users listing. */
@@ -13,9 +14,24 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
   const id = parseInt(req.params.id, 10);
-  axios.get(`http://localhost:3001/products/${id}`).then(resp => {
-    res.render('product-details', { product: resp.data.data })
-  });
+  axios.all([
+    axios.get(`http://localhost:3001/products/${id}`), 
+    axios.get(`http://localhost:3001/categories`)
+  ])
+  .then(axios.spread((productResp, categoriesResp) => {
+    const product = productResp.data.data;
+    const categories = categoriesResp.data.data;
+    res.render('product-details', {
+      product,
+      categories
+    })
+    // console.log('Product:', productResp);
+    // console.log('Categories:', categoriesResp);
+  }));
+});
+
+hbs.registerHelper('isSelectedCategory', function (category, selectedCategory) {
+  return category.id === selectedCategory.id;
 });
 
 // router.get('/new', function(req, res, next) {
